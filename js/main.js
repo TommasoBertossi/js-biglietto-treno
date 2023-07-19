14e8f4843cfa6761e9d02cc69ca49bb79bc0e4e8
TommasoBertossi <tommasobertossi@gmail.com>
2023-07-19 18:32:11 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const kilometers = parseInt(prompt("Quanti chilometri devi percorrere?"));
 const age = parseInt(prompt("Quanti anni hai?"));
 const fullPrice = kilometers * 0.21;
+let inizialPhrase = ("Il prezzo del tuo biglietto è di ");
+let price;
 
-if (isNaN(kilometers) || isNaN(age) {
+if(isNaN(kilometers) || isNaN(age)) {
     alert('Errore! Scrvi i dati in numeri.');
     }else if(age < 18) {
-        let price = fullPrice - (fullPrice / 100) * 20;
+        price = fullPrice - (fullPrice / 100) * 20;
     }else if(age > 65) {
-        let price = fullPrice - (fullPrice / 100) * 40;
+        price = fullPrice - (fullPrice / 100) * 40;
     } else price = fullPrice;
 
+price = price.toFixed(2);
+const screenPrint = inizialPhrase + price
+
+document.getElementById("ticketPrice").innerHTML = screenPrint;
